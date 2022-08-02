3bc3c35cb583fa9f548e679c21826754e086b27f
alexstorm <solenopsys:)>
2022-08-03 02:45:40 +0300

    fix build

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 build_push(){
-  docker buildx build  --platform ${ARCHS} -t ${REGISTRY}/public-api:latest  --output=type=registry,registry.insecure=true --push .
+  docker buildx build  --platform ${ARCHS} -t ${REGISTRY}/${NAME}:latest   --push .
 }
 
 helm_build_push(){
   FN=${NAME}-${VER}.tgz
   # rm ${FN}
   helm package ./install --version ${VER}
-  curl --data-binary "@${FN}" http://helm.local/api/charts
+  curl --data-binary "@${FN}" http://helm.alexstorm.solenopsys.org/api/charts
 }
 
-REGISTRY=registry.local
-NAME=public-api
+REGISTRY=registry.alexstorm.solenopsys.org
+NAME=alexstorm-public-api
 ARCHS="linux/amd64,linux/arm64"
-VER=0.1.4
+VER=0.1.0
 
 
 helm_build_push
